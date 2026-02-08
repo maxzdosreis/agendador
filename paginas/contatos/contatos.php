4483e4ec7a9ad57f39252071c0b3a06f3f71bab4
Max Zimmermann dos Reis <maxzdosreis@gmail.com>
2026-02-08 15:27:14 -0300

    feat: aplica paginação na página de contatos
    - Adiciona métodos e cálculos necessários para que a paginação ocorra corretamente

--- paginas/contatos/contatos.php
+++ paginas/contatos/contatos.php
@@ -28,6 +28,13 @@ include_once('db/conexao.php');
         </tr>
     </thead>
     <?php 
+
+    $quantidade = 10;
+
+    $pagina = (isset($_GET['pagina']))? (int)$_GET['pagina'] : 1;
+
+    $inicio = ($quantidade * $pagina) - $quantidade;
+
     $txt_pesquisa = (isset($_POST['txt_pesquisa'])) ? $_POST['txt_pesquisa'] : '';
 
     $sql = "
@@ -49,7 +56,8 @@ include_once('db/conexao.php');
             idContato = '{$txt_pesquisa}'
             OR nomeContato LIKE '%{$txt_pesquisa}%'
             ORDER BY nomeContato ASC
-           ";
+            LIMIT $inicio, $quantidade
+        ";
     $result = mysqli_query($conexao,$sql) or die ("Erro ao executar a consulta!" . mysqli_error($conexao));
     // Percorre linha por linha, e guarda os dados
     while($dados = mysqli_fetch_assoc($result)) {
@@ -71,3 +79,37 @@ include_once('db/conexao.php');
     }
     ?>
 </table>
+<br>
+<?php  
+
+$sqlTotal = "SELECT idContato FROM tbcontatos";
+$resultTotal = mysqli_query($conexao,$sqlTotal) or die ("Não foi possível realizar a consulta no banco de dados ". mysqli_error($conexao));
+$numTotal = mysqli_num_rows($resultTotal);
+$totalPagina = ceil($numTotal/$quantidade);
+echo "Total de registros: $numTotal <br>";
+echo '<a href="?menuop=contatos&pagina=1">Primeira Página</a>';
+
+if($pagina>6) {
+    ?>
+        <a href="?menuop=contatos&pagina=<?php echo $pagina - 1?>"> << </a>
+    <?php
+}
+
+for($i = 1; $i<=$totalPagina; $i++) {
+    if($i>=($pagina-5) && $i<($pagina+5)) {
+        if($i== $pagina) {
+            echo $i;
+        } else {
+            echo "<a href=\"?menuop=contatos&pagina=$i\">$i</a>  ";
+        }
+    }
+}
+
+if($pagina < ($totalPagina-5)) {
+    ?>
+        <a href="?menuop=contatos&pagina=<?php echo $pagina + 1?>"> >> </a>
+    <?php
+}
+
+echo "<a href=\"?menuop=contatos&pagina=$totalPagina\">Última Página</a>";
+?>
