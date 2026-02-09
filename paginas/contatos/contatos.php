2396cbec0507f9c0b8cff76705f546065ecb3ab7
max-reis <162522633+maxzdosreis@users.noreply.github.com>
2026-02-09 00:24:37 -0300

    feat/bootstrap
    * feat: adiciona bootstrap no projeto
    - Adiciona imagens do projeto do diretório
    - estiliza index.php com estilizações básicas
    - adiciona customização própria do body
    
    * feat: estiliza as tabelas
    - Realiza a estilização das tabelas do projeto com bootstrap
    
    * feat: estiliza paginação
    - Adiciona estilização no processo de paginação no fim da página de contatos
    
    * feat: adiciona estilização de ícones e botões
    - Estiliza os botões e ícones do projeto com o bootstrap
    
    * feat: adiciona estilização nos formulários do projeto
    - Estiliza os formulários do projeto com bootstrap

--- paginas/contatos/contatos.php
+++ paginas/contatos/contatos.php
@@ -2,114 +2,119 @@
 include_once('db/conexao.php');
 ?>
 <header>
-    <h3>Contatos</h3>
+    <h3><i class="bi bi-person-square"></i> Contatos</h3>
 </header>
 <div>
-    <a href="index.php?menuop=cad-contato">Novo Contato</a>
+    <a class="btn btn-outline-secondary mb-2" href="index.php?menuop=cad-contato"><i class="bi bi-person-plus-fill"></i> Novo Contato</a>
 </div>
 <div>
     <form action="index.php?menuop=contatos" method="post">
-        <input type="text" name="txt_pesquisa">
-        <input type="submit" name="Pesquisar">
+        <div class="input-group">
+            <input class="form-control" type="text" name="txt_pesquisa">
+            <button class="btn btn-outline-success btn-sm" type="submit"><i class="bi bi-search"></i> Pesquisar</button>
+        </div>
     </form>
 </div>
-<table border="1">
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>E-mail</th>
-            <th>Telefone</th>
-            <th>Endereço</th>
-            <th>Sexo</th>
-            <th>Data de Nasc.</th>
-            <th>Editar</th>
-            <th>Excluir</th>
-        </tr>
-    </thead>
-    <?php 
+<div class="tabela">
+    <table class="table table-dark table-hover table-bordered">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>E-mail</th>
+                <th>Telefone</th>
+                <th>Endereço</th>
+                <th>Sexo</th>
+                <th>Data de Nasc.</th>
+                <th>Editar</th>
+                <th>Excluir</th>
+            </tr>
+        </thead>
+        <?php 
 
-    $quantidade = 10;
+        $quantidade = 10;
 
-    $pagina = (isset($_GET['pagina']))? (int)$_GET['pagina'] : 1;
+        $pagina = (isset($_GET['pagina']))? (int)$_GET['pagina'] : 1;
 
-    $inicio = ($quantidade * $pagina) - $quantidade;
+        $inicio = ($quantidade * $pagina) - $quantidade;
 
-    $txt_pesquisa = (isset($_POST['txt_pesquisa'])) ? $_POST['txt_pesquisa'] : '';
+        $txt_pesquisa = (isset($_POST['txt_pesquisa'])) ? $_POST['txt_pesquisa'] : '';
 
-    $sql = "
-            SELECT
-                idContato,
-                upper(nomeContato) as nomeContato,
-                lower(emailContato) as emailContato,
-                telefoneContato,
-                upper(enderecoContato) as enderecoContato,
-                CASE
-                    WHEN sexoContato='F' THEN 'FEMININO'
-                    WHEN sexoContato='M' THEN 'MASCULINO'
-                ELSE
-                    'NÃO ESPECIFICADO'
-                END AS sexoContato,
-                DATE_FORMAT(dataNascContato, '%d/%m/%Y') as dataNascContato
-            FROM tbcontatos
-            WHERE 
-            idContato = '{$txt_pesquisa}'
-            OR nomeContato LIKE '%{$txt_pesquisa}%'
-            ORDER BY nomeContato ASC
-            LIMIT $inicio, $quantidade
-        ";
-    $result = mysqli_query($conexao,$sql) or die ("Erro ao executar a consulta!" . mysqli_error($conexao));
-    // Percorre linha por linha, e guarda os dados
-    while($dados = mysqli_fetch_assoc($result)) {
-    ?>
-    <tbody>
-        <tr>
-            <td><?=$dados['idContato']?></td>
-            <td><?=$dados['nomeContato']?></td>
-            <td><?=$dados['emailContato']?></td>
-            <td><?=$dados['telefoneContato']?></td>
-            <td><?=$dados['enderecoContato']?></td>
-            <td><?=$dados['sexoContato']?></td>
-            <td><?=$dados['dataNascContato']?></td>
-            <td><a href="index.php?menuop=editar-contato&idContato=<?=$dados['idContato']?>">Editar</a></td>
-            <td><a href="index.php?menuop=excluir-contato&idContato=<?=$dados['idContato']?>">Excluir</a></td>
-        </tr>
-    </tbody>
-    <?php  
-    }
-    ?>
-</table>
-<br>
+        $sql = "
+                SELECT
+                    idContato,
+                    upper(nomeContato) as nomeContato,
+                    lower(emailContato) as emailContato,
+                    telefoneContato,
+                    upper(enderecoContato) as enderecoContato,
+                    CASE
+                        WHEN sexoContato='F' THEN 'FEMININO'
+                        WHEN sexoContato='M' THEN 'MASCULINO'
+                    ELSE
+                        'NÃO ESPECIFICADO'
+                    END AS sexoContato,
+                    DATE_FORMAT(dataNascContato, '%d/%m/%Y') as dataNascContato
+                FROM tbcontatos
+                WHERE 
+                idContato = '{$txt_pesquisa}'
+                OR nomeContato LIKE '%{$txt_pesquisa}%'
+                ORDER BY nomeContato ASC
+                LIMIT $inicio, $quantidade
+            ";
+        $result = mysqli_query($conexao,$sql) or die ("Erro ao executar a consulta!" . mysqli_error($conexao));
+        // Percorre linha por linha, e guarda os dados
+        while($dados = mysqli_fetch_assoc($result)) {
+        ?>
+        <tbody>
+            <tr>
+                <td><?=$dados['idContato']?></td>
+                <td class="text-nowrap"><?=$dados['nomeContato']?></td>
+                <td class="text-nowrap"><?=$dados['emailContato']?></td>
+                <td class="text-nowrap"><?=$dados['telefoneContato']?></td>
+                <td><?=$dados['enderecoContato']?></td>
+                <td><?=$dados['sexoContato']?></td>
+                <td><?=$dados['dataNascContato']?></td>
+                <td class="text-center"><a class="btn btn-outline-warning btn-sm"href="index.php?menuop=editar-contato&idContato=<?=$dados['idContato']?>"><i class="bi bi-pencil-square"></i></a></td>
+                <td class="text-center"><a class="btn btn-outline-danger btn-sm" href="index.php?menuop=excluir-contato&idContato=<?=$dados['idContato']?>"><i class="bi bi-trash-fill"></i></a></td>
+            </tr>
+        </tbody>
+        <?php  
+        }
+        ?>
+    </table>
+</div>
+
+<ul class="pagination justify-content-center">
 <?php  
-
 $sqlTotal = "SELECT idContato FROM tbcontatos";
 $resultTotal = mysqli_query($conexao,$sqlTotal) or die ("Não foi possível realizar a consulta no banco de dados ". mysqli_error($conexao));
 $numTotal = mysqli_num_rows($resultTotal);
 $totalPagina = ceil($numTotal/$quantidade);
-echo "Total de registros: $numTotal <br>";
-echo '<a href="?menuop=contatos&pagina=1">Primeira Página</a>';
+echo "<li class='page-item'><span class='page-link'>Total de registros: " . $numTotal . " </span></li>";
+echo '<li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=1">Primeira Página</a></li>';
 
 if($pagina>6) {
     ?>
-        <a href="?menuop=contatos&pagina=<?php echo $pagina - 1?>"> << </a>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina - 1?>"> << </a></li>
     <?php
 }
 
 for($i = 1; $i<=$totalPagina; $i++) {
     if($i>=($pagina-5) && $i<($pagina+5)) {
         if($i== $pagina) {
-            echo $i;
+            echo "<li class='page-item active'><span class='page-link'>$i</span></li>";
         } else {
-            echo "<a href=\"?menuop=contatos&pagina=$i\">$i</a>  ";
+            echo "<li class='page-item'><a class='page-link' href=\"?menuop=contatos&pagina={$i}\">{$i}</a></li>  ";
         }
     }
 }
 
 if($pagina < ($totalPagina-5)) {
     ?>
-        <a href="?menuop=contatos&pagina=<?php echo $pagina + 1?>"> >> </a>
+        <li class="page-item"><a class="page-link" href="?menuop=contatos&pagina=<?php echo $pagina + 1?>"> >> </a></li>
     <?php
 }
 
-echo "<a href=\"?menuop=contatos&pagina=$totalPagina\">Última Página</a>";
+echo "<li class='page-item'><a class='page-link' href=\"?menuop=contatos&pagina=$totalPagina\">Última Página</a></li>";
 ?>
+</ul>
